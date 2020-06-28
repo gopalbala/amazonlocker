6b791ca9b969c48fd869e7b240c42899dd6bbb12
gopalbala <balasubramaniangopalakrishnan@gmail.com>
2020-06-28 09:55:15 +0530

    added order repository

---
+++ src/main/java/com/gb/amazonlocker/repository/OrderRepository.java
@@ -0,0 +1,14 @@
+package com.gb.amazonlocker.repository;
+
+import com.gb.amazonlocker.model.Order;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class OrderRepository {
+    public static Map<String, Order> orderMap = new HashMap<>();
+
+    public Order getOrder(String orderId) {
+        return orderMap.get(orderId);
+    }
+}
